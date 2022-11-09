e9ffe6a7e6580d63b8b62b299503319f39ba8843
Renze Yu <superyyrrzz@gmail.com>
2022-11-09 15:59:07 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ if (process.argv.length < 4) {
   console.log("useage: node test {docfxJsPreprocessor} {rawModel}");
   process.exit();
 }
+
 var script = require('./' + process.argv[2]);
 var fs = require('fs');
 var json = JSON.parse(fs.readFileSync(process.argv[3]));
